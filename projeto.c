71df58ad8b28e62cc6a5d8d8f71429112df3498c
Marcelo-Chaves <111310959+Marcelo-Chaves@users.noreply.github.com>
2024-07-14 18:31:56 -0400

    Add files via upload

--- projeto.c
+++ projeto.c
@@ -3,6 +3,102 @@
 # include <locale.h> // biblioteca de alocacao de texto por regiao 
 # include <string.h> // biblioteca responsavel pela strings
 
+void voltar() // chamando a funcao para uma novo registro 
+{
+    char voltar_opcao;// variavel
+    // inicio do menu
+    printf("Para voltar ao menu principal, digite 'm'\n");
+    printf("Para fazer um novo registro, digite 'r'\n");
+    printf("Para sair do programa, digite 's'\n");
+	// fim do menu
+    scanf(" %c", &voltar_opcao);// nao deixar espaços
+
+    switch (voltar_opcao) 
+	{   
+        case 'm': // Voltar ao menu principal
+        return;
+    
+        case 'r': // Fazer novo registro
+        registro();
+        break;
+        
+        case 's': // Sair do programa
+        printf("obrigado por utilizar nosso sistema");
+        exit(0);
+        
+        default:
+            printf("Opção inválida.\n");
+            break;
+    }
+    
+}    
+void voltar2() // chamando a funcao para uma nova consulta
+{
+    char voltar2_opcao; // criando uma variavel
+    // inicio do menu
+    printf("Para voltar ao menu principal, digite 'm' para menu\n");
+    printf("Para fazer um nova Constulta, digite 'c' nova consulta\n");
+    printf("Para sair do programa, digite 's' para sair\n");
+ 	// fim do menu
+    scanf(" %c", &voltar2_opcao);
+
+    switch (voltar2_opcao) 
+	{   
+        case 'm': // Voltar ao menu principal
+        return;
+    
+        case 'c': // Fazer nova consulta
+        consultar();
+        break;
+        
+        case 's': // Sair do programa
+        printf("obrigado por utilizar nosso sistema");
+        exit(0);
+        
+        default:
+        printf("Opção inválida.\n");
+        break;
+    }
+
+    while (getchar() != '\n');// Limpar o buffer de entrada para evitar problemas com entradas não esperadas
+
+    system("pause"); // Aguardar que o usuário veja a mensagem
+} 
+void voltar3() // chamando a funcao para uma nova consulta
+{
+    char voltar3_opcao; // criando uma variavel
+    // inicio do menu
+    printf("Para voltar ao menu principal, digite 'm' para menu\n");
+    printf("Para fazer um nova Constulta, digite 'd' nova consulta\n");
+    printf("Para sair do programa, digite 's' para sair\n");
+ 	// fim do menu
+    scanf(" %c", &voltar3_opcao);
+
+    switch (voltar3_opcao) 
+	{   
+        case 'm': // Voltar ao menu principal
+        return;
+    
+        case 'd': // Fazer nova consulta
+        deletar();
+        break;
+        
+        case 's': // Sair do programa
+        printf("obrigado por utilizar nosso sistema");
+        exit(0);
+        
+        default:
+        printf("Opção inválida.\n");
+        break;
+    }
+
+    while (getchar() != '\n');// Limpar o buffer de entrada para evitar problemas com entradas não esperadas
+
+    system("pause"); // Aguardar que o usuário veja a mensagem
+} 
+
+
+
 int registro() // funcao responsavel por cadastrar ususarios 
 {
 	printf("Voce escolheu registro de nomes! \n\n"); // escolha feita pelo usuario
@@ -14,6 +110,7 @@ int registro() // funcao responsavel por cadastrar ususarios
 	char nome [40];
 	char sobrenome [40];
 	char cargo [40];
+
 	//fim da criacao de variaiveis/string
 	
 	printf("Digite o CPF a ser Cadastrado: \n"); //Coletado informacao do usuario
@@ -39,42 +136,44 @@ int registro() // funcao responsavel por cadastrar ususarios
 	
 	file = fopen(arquivo,"a");
 	fprintf(file,",");
-	fclose(file); responsavel por fechar arquivo
+	fclose(file); //responsavel por fechar arquivo
 	
 	printf("Digite o Sobrenome a ser cadastrado! \n");//Coletado informacao do usuario
 	scanf("%s",sobrenome);
 	
 	file = fopen(arquivo,"a");// responsavel por abrir arquivo
-	fprintf (file,sobrenome);
+	fprintf (file,sobrenome);// responsavel por entrar arquivo
 	fclose(file); // responsavel por fechar arquivo
 	
 	file = fopen(arquivo,"a");// responsavel por abrir arquivo
-	fprintf(file,",");
-	fclose(file); responsavel por fechar arquivo
+	fprintf(file,",");// responsavel por entrar arquivo
+	fclose(file); //responsavel por fechar arquivo
 	
 	printf("Digite o cargo a ser cadastrado \n");//Coletado informacao do usuario
 	scanf("%s",cargo);
 	
 	file = fopen(arquivo,"a");// responsavel por abrir arquivo
-	fprintf(file,cargo);
-	fclose(file); responsavel por fechar arquivo
+	fprintf(file,cargo);// responsavel por entrar arquivo
+	fclose(file); //responsavel por fechar arquivo
 	
 	file = fopen(arquivo,"a"); // responsavel por abrir arquivo
-	fprintf (file,",");
-	fclose(file); responsavel por fechar arquivo
+	fprintf (file,",");// responsavel por entrar arquivo
+	fclose(file);// responsavel por fechar arquivo
 	
-	system("pause");
-	   
+	printf("Cadastro realizado com sucesso\n\n");
 }
+
 int consultar() // funcao responsavel por consultar ususarios 
 {
 		setlocale(LC_ALL, "PORTUGUESE"); //definindo a linguagem
 	
 		printf("Voce escolheu consultar nomes!\n\n"); // escolha feita pelo usuario
-		system("pause");
+		system("pause");// refere-se em dar tempo de leitura ao usuario 
 	
+		//inicio da criacao de variaiveis/string
 		char cpf[40];
 		char conteudo[200];
+		//inicio da criacao de variaiveis/string
 	
 		printf("Digite o cpf a ser Consultado: \n "); // responsavel por escrever na tela
 		scanf("%s",cpf ); // %s refere a string
@@ -94,7 +193,7 @@ int consultar() // funcao responsavel por consultar ususarios
 		printf("\n\n");
 	}
 	
-	system("pause");
+	system("pause");// refere-se em dar tempo de leitura ao usuario 
 }
 int deletar() // funcao responsavel por deletar ususarios 
 {
@@ -112,10 +211,10 @@ int deletar() // funcao responsavel por deletar ususarios
 	 FILE *file;
 	 file = fopen(cpf,"r");// responsavel por abrir arquivo
 	 
-	 if(file == NULL) 
+	 if(file == NULL) // responsavel por dizer que nao existe
 	 {
 	 	printf("o usuario nao se encontra no sistema. \n");// responsavel por dar a mensagem de usuario nao existe no sistema
-	 	system("pause"); // // refere-se em dar tempo de leitura ao usuario 
+	 	system("pause"); // refere-se em dar tempo de leitura ao usuario 
 	 }  
 }
 
@@ -145,16 +244,19 @@ int main()
 		{
 			case 1: 
 			registro(); // chamando a funcao registro  
-			break;
+			voltar(); // chamando a funcao voltar 
+			break;// chamando a funcao parar
 			
 			case 2:
-			consultar(); // chamando a funcao consultar 
-			break;
+			consultar(); // chamando a funcao consultar
+			voltar2(); // chamando a funcao para uma nova consulta
+			break;// chamando a funcao parar
 			
 			case 3:
 			deletar(); // chamando a funcao deletar
-			break;
-			
+			voltar3();// chamando a funcao voltar
+			break;// chamando a funcao parar
+		
 			default:
 			printf("Esta funcao nao esta disponivel \n\n");
 			system("pause");
@@ -162,4 +264,5 @@ int main()
 				
 		}		
     }	
+    
 }
